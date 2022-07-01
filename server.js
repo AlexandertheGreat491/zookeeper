92d321ac343c83d5194901fb7c18a28d32a050c9
agvan <agvandyke25@gmail.com>
2022-07-01 11:43:47 -0500

    added app.get & send() method & accessed api

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const {animals} = require('./data/animals');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.get('/api/animals', (req, res) => {
+  res.send('Hello!');
+})
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
