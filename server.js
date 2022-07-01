ad03b02e847408c174ff715283853247b412749e
agvan <agvandyke25@gmail.com>
2022-07-01 11:10:00 -0500

    added code

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 //express.js require
-const app = express();
+const express = require('express');
 //will later chain methods to the Express.js server
 
+const { animals } = require('./data/animals');
+
+app.listen();
+app.get('/api/animals', (req, res) => {
+  res.send('Hello!');
+});
+
 app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+  console.log(`API server now on port 3001!`);
 });
