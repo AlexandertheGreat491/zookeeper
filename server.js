c5f36bf43dc9b35330da61271d324e895ada26a2
agvan <agvandyke25@gmail.com>
2022-07-01 11:17:33 -0500

    updated code as the module said

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 //express.js require
-const express = require('express');
+const app = require('express');
 //will later chain methods to the Express.js server
 
-const { animals } = require('./data/animals');
 
-app.listen();
-app.get('/api/animals', (req, res) => {
-  res.send('Hello!');
-});
+
+
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
