1ebff97e306121c528e438d4ffdf979f077ddfd0
agvan <agvandyke25@gmail.com>
2022-07-01 12:27:41 -0500

    added filterByQuery function with arrays to pull json from api

--- server.js
+++ server.js
@@ -7,10 +7,45 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/animals', (req, res) => {
-  res.json(animals);
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
 });
 
-
+function filterByQuery(query, animalsArray) {
+  let personalityTraitsArray = [];
+  // Note that we save the animalsArray as filteredResults here:
+  let filteredResults = animalsArray;
+  if (query.personalityTraits) {
+    // Save personalityTraits as a dedicated array.
+    // If personalityTraits is a string, place it into a new array and save.
+    if (typeof query.personalityTraits === 'string') {
+      personalityTraitsArray = [query.personalityTraits];
+    } else {
+      personalityTraitsArray = query.personalityTraits;
+    }
+    // Loop through each trait in the personalityTraits array:
+    personalityTraitsArray.forEach(trait => {
+      
+      filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+      );
+    });
+  }
+  if (query.diet) {
+    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+  }
+  // return the filtered results:
+  return filteredResults;
+}
 
 app.listen(3001, () => {
   console.log(`API server now on port 3001!`);
